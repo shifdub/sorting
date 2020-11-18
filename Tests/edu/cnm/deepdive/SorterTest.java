a03db09b6a49c81030c19eb312d459cec312469c
isaac <shifdub@gmail.com>
2020-11-18 11:24:49 -0700

    Testing and bug fixing completed

--- Tests/edu/cnm/deepdive/SorterTest.java
+++ Tests/edu/cnm/deepdive/SorterTest.java
@@ -2,9 +2,41 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class SorterTest {
 
-  @org.junit.jupiter.api.Test
-  void sort() {
+  private static Stream<Arguments> sort() {
+    Random rng = new Random();
+    return Stream.generate(() -> singleCase(rng, 50))
+        .limit(10);
   }
+
+  private static Arguments singleCase(Random rng, int length) {
+    int[] data = IntStream.generate(rng::nextInt)
+        .limit(length)
+        .toArray();
+    int[] expected = Arrays.copyOf(data, data.length);
+    Arrays.sort(expected);
+    return Arguments.of(data, expected);
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void sort(int[] input, int[] expected) {
+    int[] mergeInput = Arrays.copyOf(input, input.length);
+    int[] quickInput = Arrays.copyOf(input, input.length);
+    new MergeSorter().sort(mergeInput);
+    assertArrayEquals(expected, mergeInput);
+    new QuickSorter().sort(quickInput);
+    assertArrayEquals(expected, quickInput);
+  }
+
+
 }
--- src/edu/cnm/deepdive/QuickSorter.java
+++ src/edu/cnm/deepdive/QuickSorter.java
@@ -9,12 +9,12 @@ public class QuickSorter implements Sorter {
       int pivotValue = data[pivotPosition];
       data[pivotPosition] = data[lowerBound];
       data[lowerBound] = pivotValue;
-      int partitionIndex = lowerBound;
+      int partitionIndex = lowerBound + 1;
       for (int currentIndex = lowerBound; currentIndex < upperBound; currentIndex++) {
         int currentValue = data[currentIndex];
         if (currentValue < pivotValue) {
           if (currentIndex > partitionIndex) {
-            data[currentValue] = data[partitionIndex];
+            data[currentIndex] = data[partitionIndex];
             data[partitionIndex] = currentValue;
           }
           partitionIndex++;
@@ -22,7 +22,7 @@ public class QuickSorter implements Sorter {
       }
       data[lowerBound] = data[partitionIndex - 1];
       data[partitionIndex - 1] = pivotValue;
-      sort(data, lowerBound, partitionIndex -1);
+      sort(data, lowerBound, partitionIndex - 1);
       sort(data, partitionIndex, upperBound);
     }
   }
